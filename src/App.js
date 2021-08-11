53f9c499a9aede240e6d843f14c68d110586057d
James <jaames.chen@gmail.com>
2021-08-10 21:11:25 -0400

    added bootstrap and router dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router} from "react-router-dom"
+// import MainContainer from "./containers/MainContainer";
 import {useEffect} from 'react';
 
 
@@ -14,22 +16,11 @@ function App() {
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* <MainContainer/> */}test
+      </div>      
+    </Router>
   );
 }
 
